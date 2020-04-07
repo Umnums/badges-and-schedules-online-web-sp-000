813f51102c608d06a446ebbace2eb906bd006df5
Joseph Kao <josephtkao@gmail.com>
2020-04-07 17:23:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name.}"
+end
+
+def batch_badge_creator(array)
+  newarray = []
+  array.each do |name|
+    newarray << badge_maker(name)
+  end
+  newarray
+end
+
+def assign_rooms(array)
+  newarray = []
+  array.each_with_index do |name, index|
+    newarray << "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+  newarray
+end
